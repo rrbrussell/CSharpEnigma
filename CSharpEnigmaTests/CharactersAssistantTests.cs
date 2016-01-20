3a4b32e18c13daa7a7863fbc83905e30ba0020cf
Robert R. Russell <robert@rrbrussell.com>
2016-01-19 20:35:51 -0600

    Started adding tests.

---
+++ CSharpEnigmaTests/CharactersAssistantTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpEnigma;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEnigma.Tests
+{
+    [TestClass()]
+    public class CharactersAssistantTests
+    {
+        [TestMethod()]
+        public void nextCharacterTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
